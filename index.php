a089b42e48e61c7bedc2a609a6b0f1407023dcd0
hanifaslamm <aslamhanif141@gmail.com>
2023-01-17 19:23:32 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut doloremque sed neque quisquam quae, voluptatem atque quidem quo veniam vel eum, laborum amet facere eos aliquid dolor? Iste, accusamus nihil.</p>
 </body>
 </html>
