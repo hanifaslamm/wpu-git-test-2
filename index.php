24bd85dd59b87bc3dc43180b7ddf9ab129aec06f
hanifaslamm <aslamhanif141@gmail.com>
2023-01-17 19:28:56 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut doloremque sed neque quisquam quae, voluptatem atque quidem quo veniam vel eum, laborum amet facere eos aliquid dolor? Iste, accusamus nihil.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
